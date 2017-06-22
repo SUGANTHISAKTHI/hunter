49d214361ce1ea2423c923e3f2efdd1c62a28ee1
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-06-22 10:29:51 +0530

    Add files via upload

---
+++ reverseword.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class reverseword {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the two String");
+		String str1=s.nextLine();
+		String str2=s.nextLine();
+		System.out.println("Two String reverse are");
+		System.out.println(str2+" "+str1);
+	}
+
+}
