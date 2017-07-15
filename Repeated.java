3e0de9931286b35cd62a1e7b96313ba2982da39a
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-07-15 19:23:11 +0530

    Add files via upload

--- Repeated.java
+++ Repeated.java
@@ -1,23 +1,50 @@
-package guvi;
-import java.util.Scanner;
+package hunter;
+import java.util.*;
 public class Repeated {
-
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the size of the array");
-		int n=s.nextInt();
-		int[] a=new int[n];
-		int i,j,sa;
-		int[] b=new int[n];
-		for(i=0;i<n;i++){
-			a[i]=s.nextInt();
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the string");
+	String str1=s.next();
+	int count=0;
+	int len=str1.length();
+	String str2=" ";
+	StringBuffer str3=new StringBuffer(str1);
+	str3.reverse();
+	String str=str3.toString();
+	char chone=' ';
+	int oneposition=0;
+	for(int k=0;k<len;k++){
+		count=0;
+		char ch2=str.charAt(k);
+		for(int m=0;m<len;m++){
+			char ch3=str.charAt(m);
+			if(ch2==ch3){
+			count=count+1;
+			}
 		}
-		for(i=0;i<n;i++){
-        if(a[i]==a[i+1]){
-	System.out.println(a[i]);
-}
-			 
-}
+		if(count==1){
+			chone=ch2;
+			oneposition=k;
+		}
+	}
+	for(int i=0;i<len;i++){
+		char ch=str.charAt(i);
+		count=1;
+		for(int j=i+1;j<len;j++){
+			char ch1=str.charAt(j);
+			//System.out.println(ch+" "+ch1);
+			if(ch==ch1){
+				count=count+1;
+			}
+		}
+		if(count>=2 && count<=2){
+			str2=str2+ch;
+		}else if(oneposition==i){
+			str2=str2+chone;
+		}
+	}
+	System.out.println(str2);
+
 	}
 
 }
